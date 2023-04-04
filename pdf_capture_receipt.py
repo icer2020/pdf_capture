0507d6cb668743a181abdb4e0f858538d8d19f2e
icer2020 <foxoon_lee@163.com>
2023-04-04 16:21:21 +0800

    1st pass version

--- pdf_capture_receipt.py
+++ pdf_capture_receipt.py
@@ -1,74 +1,76 @@
-# import pdfplumber
-# with pdfplumber.open(r'1.pdf') as pdf:
-#     first_page = pdf.pages[0]
-#     # print(first_page.extract_text())
-#     print(first_page.extract_tables())
-
-
-'''
-import os
-import fitz
-import shutil
-#     支持滴滴行程单。
-#     # PyMuPDF模块：https://pymupdf.readthedocs.io/en/latest/tutorial.html#extracting-text-and-images
-root_dir = "./"
-out_dir = "./output"
-if not os.path.exists(out_dir):
-    os.mkdir(out_dir)
- 
- 
-def fapiao_read(text):
-    if "￥" in text:
-        print(text.split("￥")[-1].split("\n")[0])
-        money = text.split("￥")[-1].split("\n")[0]
-    elif "¥" in text:
-        print(text.split("¥")[-1].split("\n")[0])
-        money = text.split("¥")[-1].split("\n")[0]
-    return money
- 
- 
-def xingchengdan_read(text):
-    print(text.split("合计")[1].split("元")[0].split()[0])
-    money = text.split("合计")[1].split("元")[0].split()[0]
-    return money
- 
- 
-Repeat_name_list = []
-for file in os.listdir("./"):
-    if file.endswith(".pdf"):
-        src = os.path.join(root_dir, file)
-        doc = fitz.open(src)  # or fitz.Document(filename)
-        page = doc.load_page(0)
-        text = page.get_text("text")
-        print(text)
- 
-        if "￥" in text or "¥" in text:
-            if "敏感信息脱敏" in text and "敏感信息脱敏" in text:
-                money = fapiao_read(text)
-                out_file_name = "电子发票" + money + "元"
-            else:
-                continue
-        else:
-            money = xingchengdan_read(text)
-            out_file_name = "电子发票" + money + "元行程单"
-        Repeat_name_list.append(out_file_name)
-        if out_file_name in Repeat_name_list:
-            repeat_num = Repeat_name_list.count(out_file_name)
-            if repeat_num == 1:
-                out_file_name = out_file_name
-            else:
-                out_file_name = out_file_name + "(" + str(repeat_num-1) + ")"
-        dst = os.path.join(out_dir, out_file_name+".pdf")
-        shutil.copy(src, dst)
-'''
-
 import camelot.io as camelot
-# import Camelot.io as Camelot
-receipt = "1.pdf"
-tables = camelot.read_pdf(receipt, shift_text=[''], strip_text='\n')
-table = tables[0].df  # 将发票表格转换为dataframe
-price = table[10][2][1:]
-if len(price) == 0:
-    price = table[9][2][-6:]
+import os
+# import pprint
+# import shutil
 
-print(price)
+fo_file = 'summary.txt'
+fo_sum = open(fo_file, 'w', encoding="utf-8")
+def seek_files(id1, name):
+    file_name = []
+    for root, dirs, files in os.walk(id1):
+        for fn in files:
+            if ".pdf" in fn:
+                fn_full = root + "/" + fn
+                file_name.append(fn_full)
+    return file_name
+
+
+file_name = seek_files("pdf", "pdf")
+
+sum = 0
+file_price = dict()
+for receipt in file_name:
+    price = "NA"
+    # shutil.copy(f, receipt)
+    print("-I-: parse file:", receipt)
+    tables = camelot.read_pdf(receipt, shift_text=[''], strip_text='\n')
+    table = tables[0].df  # 将发票表格转换为dataframe
+    # price = table[10][2][1:]
+    if len(table.columns) == 6:
+        clm = len(table.columns) - 1 
+    else:
+        clm = len(table.columns) - 2 
+    price = table[clm][2][1:]
+    # print("==", table[clm][2].split())
+    # print('1-price', price)
+    try:
+        price = float(price)
+    except:
+        price = table[clm][2][5:]
+
+    try:
+        price = float(price)
+    except:
+        print("-E- file {:} price is not float/int please check".format(receipt))
+
+    # if len(price) == 0:
+    #     price = table[9][2][-6:]
+    #     print('2-price', price)
+    sum += float(price)
+    file_price[receipt] = price
+
+
+outline = "{:<60}{:>20}".format("file", "price")
+# print(outline)
+fo_sum.writelines(outline+"\n")
+
+outline = "-"*140
+# print(outline)
+fo_sum.writelines(outline+"\n")
+
+for n in sorted(file_price.keys()):
+    outline = "{:<60}{:>20}".format(n, file_price[n])
+    # print(outline)
+    fo_sum.writelines(outline+"\n")
+
+outline = "="*140
+# print(outline)
+fo_sum.writelines(outline+"\n")
+
+outline = ("{:<60}{:>20}".format(len(file_price.keys()), sum))
+# print(outline)
+fo_sum.writelines(outline+"\n")
+
+fo_sum.close()
+
+print("-I-: total {:} file(s) are parse. Total fee are {:}. Detail is in file: {:}".format(len(file_price.keys()), sum, fo_file))
