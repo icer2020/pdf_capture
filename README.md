63cc336d9e63276f580b7d631f29df15ccb6e001
“icer2020” <foxoon_lee@163.com>
2023-04-05 21:45:34 +0800

    add

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 1: translate Hongen kid word PDF to word-card as picture format
 2: download full code zip and run
 - in windows
-python pdf_capture.py
+python pdf_capture.py -h ; # show help
+python pdf_capture.py ; # run script
 - in unix
-./pdf_capture.py
+./pdf_capture.py -h ; # show help
+./pdf_capture.py ; # run script
 
 
 
--- pdf_capture.py
+++ pdf_capture.py
@@ -1,53 +1,88 @@
+#!/usr/bin/python3 -u
+# -*- encoding: utf-8 -*-
+'''
+@File          :   pdf_capture.py
+@Time          :   2023/04/05 21:14:28
+@Author        :   ICer
+@Version       :   0.1
+@Contact       :   i_chip_backend@163.com
+@WebSite       :   https://blog.csdn.net/i_chip_backend
+@License       :   (C)Copyright 2018-2023, ICerDev
+@Description   :   1st release version
+'''
+import PIL.Image as Image
+import argparse
+import cv2
 import fitz
 import os
 import random
-import PIL.Image as Image
-import shutil # shutil全称是shell utilities
-import cv2
+import shutil
+import sys
+import time
+import utils as ut
 
-rm_temp_file = True ; # delete all intermedia file
-# rm_temp_file = False ; # keep all intermedia file
+ut.starttime = time.time()
+
+# Version history
+ver = ut.ver = 'V0.1'
+ver_date = ut.ver_date = 'Apr. 05, 2023'
+ver_des = ut.ver_des = '1st release version.'
+ver_detail_des = ut.ver_detail_des = ''' scritps release '''
+scr_des = ut.scr_des = 'PDF capture for combined word-card'
+scr_des_detail = '''
+Description: Translate HongEn basic-word PDF into word-only combined PDF for kid use
+'''
+
+
+# ------------------------------------------------------------
+# function section
+# ------------------------------------------------------------
+def parseargu():
+    global ver
+    global ver_date
+    global ver_des
+    post_msg = ver.ljust(18) + ver_des.center(40) + ver_date.rjust(18)
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        description=scr_des_detail, epilog=post_msg)
+
+    parser.add_argument('-i', '--in_file', required=False,
+                        help='Input file',
+                        default='./1-200.pdf')
+    parser.add_argument('-o', '--out_file', required=False,
+                        help='Output file',
+                        default='./combined.pdf')
+    parser.add_argument('-sta', '--status', type=str, choices=['RUN', 'DEBUG'],
+                        help=argparse.SUPPRESS)
+    parser.add_argument('-k', '--keep_temp_file', action='store_false',
+                        help='keep temp png/final pic file')
+
+    args_l = parser.parse_args()
+    return args_l
 
 
 def pyMuPDF2_fitz(pdfPath, imagePath):
-    pdfDoc = fitz.open(pdfPath) # open document
-    for pg in range(pdfDoc.page_count): # iterate through the pages
+    # open pdf
+    pdfDoc = fitz.open(pdfPath) 
+    # iterate through the pages
+    for pg in range(pdfDoc.page_count): 
         if pg % 10 ==0:
-            print("-I- check page: ", pg)
+            ut.print_info("Check page: ", pg)
         page = pdfDoc[pg]
         rotate = int(0)
-        # 每个尺寸的缩放系数为1.3，这将为我们生成分辨率提高2.6的图像
-        # 此处若是不做设置，默认图片大小为：792X612, dpi=96
-        zoom_x = 1 #(1.33333333-->1056x816)   (2-->1584x1224)
-        zoom_y = 1
-
-        zoom_x = 3 #(1.33333333-->1056x816)   (2-->1584x1224)
+        # PDF zoom ratio
+        zoom_x = 3
         zoom_y = 3
 
-        mat = fitz.Matrix(zoom_x, zoom_y).prerotate(rotate) # 缩放系数1.3在每个维度  .prerotate(rotate)是执行一个旋转
-        rect = page.rect                         # 页面大小
+        mat = fitz.Matrix(zoom_x, zoom_y).prerotate(rotate)
+        rect = page.rect
 
-        # mp = rect.tl + (rect.bl - (0,75/zoom_x)) # 矩形区域    56=75/1.3333
-        # clip = fitz.Rect(mp, rect.br)            # 想要截取的区域
-        # print("rect:", rect)
-        # print("clip:", clip)
-        # print("mp:", mp)
-        # print("rect.br:", rect.br)
-        # clip = rect
-        
-        # clip = fitz.Rect(10, 88, 101, 180)            # golden for 1st word
-        x1 = 10
-        x2 = x1 + 91
-        y1 = 88
-        y2 = y1 + (180-88)
-        # clip = fitz.Rect(x1, y1, x2, y2)            # golden for 1st word
         grid = 145
         x1 = 10
         x2 = x1 + 91
         y1 = 88 + grid
-        # y1 = 88 
         y2 = y1 + (180-88)
-        clip = fitz.Rect(x1, y1, x2, y2)            # golden for 1st word
+        clip = fitz.Rect(x1, y1, x2, y2)
 
         j = 0
         for i in range(5):
@@ -55,46 +90,28 @@ def pyMuPDF2_fitz(pdfPath, imagePath):
             x1 = 10
             x2 = x1 + 91
             y1 = 88 + grid*i
-            # y1 = 88 
             y2 = y1 + (180-88)
-            clip = fitz.Rect(x1, y1, x2, y2)            # golden for 1st word
-            pix = page.get_pixmap(matrix=mat, alpha=False, clip=clip) # 将页面转换为图像
+            clip = fitz.Rect(x1, y1, x2, y2)
+            pix = page.get_pixmap(matrix=mat, alpha=False, clip=clip)
             if not os.path.exists(imagePath):
                 os.makedirs(imagePath)
-            # pix.writePNG(imagePath+'/'+'psReport_%s.png' % pg)# store image as a PNG
             pix.save(f'{imagePath}/{str(pg).zfill(3)}_{str(i).zfill(3)}.png')
-            # str.zfill(8)
 
-   
 def image_compose():
     IMAGES_PATH = imagePath
-    IMAGES_FORMAT = ['.png']  # 图片格式
+    IMAGES_FORMAT = ['.png']
 
-    # IMAGE_SIZE_X = 91  # 每张小图片的大小
-    # IMAGE_SIZE_Y = 92  # 每张小图片的大小
-
-    IMAGE_SIZE_X = 273  # 每张小图片的大小
-    IMAGE_SIZE_Y = 273  # 每张小图片的大小
-
-    IMAGE_ROW = 9  # 图片间隔，也就是合并成一张图后，一共有几行
-    IMAGE_COLUMN = 6  # 图片间隔，也就是合并成一张图后，一共有几列
-    # space_x = 3
-    # space_y = 3
-    # margin_x = 25
-    # margin_y = 5
-
+    IMAGE_SIZE_X = 273 
+    IMAGE_SIZE_Y = 273 
+    IMAGE_ROW = 9 
+    IMAGE_COLUMN = 6
     space_x = 6
     space_y = 6
     margin_x = 120
     margin_y = 70
 
-    # 获取图片集地址下的所有图片名称
     image_names = [name for name in os.listdir(IMAGES_PATH) for item in IMAGES_FORMAT if os.path.splitext(name)[1] == item]
-    # to_image = Image.new('RGB', ( (IMAGE_COLUMN*IMAGE_SIZE_X)+space_x*(IMAGE_COLUMN-1) , (IMAGE_ROW*IMAGE_SIZE_Y)+space_y*(IMAGE_ROW-1)) #创建一个新图
-    
-    # print(len(image_names))
 
-    # image_width = 610
     image_width = 1900
     image_height = int(image_width * pow(2, 0.5))
     random.shuffle(image_names)
@@ -104,9 +121,6 @@ def image_compose():
     else:
         total_page = int(img_total_cnt/float(IMAGE_COLUMN*IMAGE_ROW)) + 1
         
-
-    # print(sorted(image_names))
-
     img_cnt = 0
     index_cnt = 1
     img_file_name = []
@@ -115,36 +129,31 @@ def image_compose():
             for x in range(IMAGE_COLUMN):
                 if img_cnt ==0:
                     to_image = Image.new('RGB', (image_width, image_height), "white")
-                    IMAGE_SAVE_PATH = 'final_' + str(index_cnt) + '.jpg'  # 图片转换后的地址
+                    IMAGE_SAVE_PATH = 'final_' + str(index_cnt) + '.jpg'
                 elif img_cnt % (IMAGE_ROW*IMAGE_COLUMN)==0:
-                    to_image.save(IMAGE_SAVE_PATH) # 保存新图
+                    to_image.save(IMAGE_SAVE_PATH)
                     p_page = "-" + str(index_cnt)+"/"+str(total_page) + "-"
                     add_page(IMAGE_SAVE_PATH, p_page)
                     img_file_name.append(IMAGE_SAVE_PATH)
-                    print("IMAGE_SAVE_PATH: {:} img_cnt: {:}".format(IMAGE_SAVE_PATH, img_cnt))
+                    ut.print_debug("IMAGE_SAVE_PATH: {:} img_cnt: {:}".format(IMAGE_SAVE_PATH, img_cnt))
                     index_cnt += 1
                     to_image = Image.new('RGB', (image_width, image_height), "white")
-                    IMAGE_SAVE_PATH = 'final_' + str(index_cnt) + '.jpg'  # 图片转换后的地址
+                    IMAGE_SAVE_PATH = 'final_' + str(index_cnt) + '.jpg'
                 point_x = margin_x + x*IMAGE_SIZE_X + x*space_x 
                 point_y = margin_y + y*IMAGE_SIZE_Y + y*space_y
-                # print("img_cnt:", img_cnt)
                 if img_cnt < img_total_cnt:
                     img_f = IMAGES_PATH + "/" + image_names[img_cnt] 
-                    # print("img_f: ", img_f)
-                    # print("point_x: {:} point_y: {:}".format(point_x, point_y))
                     to_image.paste(Image.open(img_f), (point_x, point_y))
                     img_cnt += 1
                 else:
-                    to_image.save(IMAGE_SAVE_PATH) # 保存新图
-                    # add_page(IMAGE_SAVE_PATH, index_cnt-1)
+                    to_image.save(IMAGE_SAVE_PATH)
                     p_page = "-" + str(int(index_cnt))+"/"+str(total_page) + "-"
                     add_page(IMAGE_SAVE_PATH, p_page)
                     img_file_name.append(IMAGE_SAVE_PATH)
-                    print("final save IMAGE_SAVE_PATH: {:} img_cnt: {:}".format(IMAGE_SAVE_PATH, img_cnt))
+                    ut.print_debug("final save IMAGE_SAVE_PATH: {:} img_cnt: {:}".format(IMAGE_SAVE_PATH, img_cnt))
                     break
             if img_cnt >= img_total_cnt:
                 break
-    # return to_image.save(IMAGE_SAVE_PATH) # 保存新图
     doc = fitz.open()
     for img_file in img_file_name:
         imgdoc = fitz.open(img_file)
@@ -152,51 +161,52 @@ def image_compose():
         pdf_name = str(img_file) + '.pdf'
         imgpdf = fitz.open(pdf_name, pdfbytes)
         doc.insert_pdf(imgpdf)
-    doc.save('combined.pdf')
+    doc.save(args.out_file)
     doc.close()
 
-    # delete final_*.jpg
-    if rm_temp_file:
+    if args.keep_temp_file:
         for img_file in img_file_name:
             os.remove(img_file)
     
 def add_page(img_file, number):
-    #加载背景图片
     bk_img = cv2.imread(img_file)
     size = bk_img.shape
-    w = size[1] #宽度
-    h = size[0] #高度
-
-    #在图片上添加文字信息
-    # cv2.putText(bk_img, str(number), (int(size[1]/2),int(size[0]-20)), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,0,0), 3, cv2.LINE_AA)
+    w = size[1] 
+    h = size[0]
     cv2.putText(bk_img, str(number), (int(size[1]/2-80),int(size[0]-30)), cv2.FONT_HERSHEY_COMPLEX, 1.5, (0,0,0), 3, cv2.LINE_AA)
-    # #显示图片
-    # cv2.imshow("add_text",bk_img)
-    # cv2.waitKey()
-    #保存图片
     cv2.imwrite(img_file, bk_img)
 
 
-
-
+# ------------------------------------------------------------
+# Running  main
+# ------------------------------------------------------------
 if __name__ == "__main__":
-    pdfPath = '1-200.pdf'
-    # pdfPath = '201-600.pdf'
-    # pdfPath = "601-1300.pdf"
+    ut.RUN_STATUS = 'RUN'
+    # additional meg in header for current scripts
+    ut.header(add_msg='')
+
+    args = parseargu()
+
     imagePath = 'png'
-    print("-I- remove existing path:", imagePath)
+    if not args.keep_temp_file:
+        ut.print_info("Run with keep temp png/final file")
+
+    ut.print_debug("Remove existing path:", imagePath)
     if os.path.isdir(imagePath):
         shutil.rmtree(imagePath)
 
-    print('-I- remove existing file: final_*.jpg', )
+    ut.print_debug('Remove existing file: final_*.jpg', )
     for root, dirs, files in os.walk('.'):
         for fn in files:
             if "final_" in fn and ".jpg" in fn:
                 os.remove(fn)
-
-    pyMuPDF2_fitz(pdfPath, imagePath)#指定想要的区域转换成图片
+    pyMuPDF2_fitz(args.in_file, imagePath)
     image_compose()
 
     # rm png folder
-    if rm_temp_file:
+    if args.keep_temp_file:
         shutil.rmtree(imagePath)
+
+    ut.footer()
+
+
